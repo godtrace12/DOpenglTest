585fbe5aee15259e9ab597b5789de63252033e1c
daijun <595756608@qq.com>
2021-06-14 09:29:33 +0800

    保存临时代码

---
+++ appgl/src/main/java/com/example/dj/record/MediaCodecConstant.java
@@ -0,0 +1,11 @@
+package com.example.dj.record;
+
+public class MediaCodecConstant {
+    //标记track下标
+    public static int audioTrackIndex;
+    public static int videoTrackIndex;
+    //标记MediaMuxer是否开启
+    public static boolean encodeStart;
+
+
+}
