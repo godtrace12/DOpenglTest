b176421117a72b69bd7a172b36ff06e5cecb5eb2
daijun <595756608@qq.com>
2021-11-07 21:26:36 +0800

    integrate opencv

---
+++ appgl/src/main/java/com/example/dj/appgl/opencv/OpenCVActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dj.appgl.opencv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenCVActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_c_v)
+    }
+}
